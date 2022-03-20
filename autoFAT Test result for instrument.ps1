d70f01bacf603d12b4638df1f5f312f3178aa837
chenyong-ng <98865799+chenyong-ng@users.noreply.github.com>
2022-03-21 00:18:50 +0800

    Add files via upload

--- autoFAT Test result for instrument.ps1
+++ autoFAT Test result for instrument.ps1
@@ -1,20 +1,45 @@
-$env:COMPUTERNAME
+"Instrument Name: $env:COMPUTERNAME"
 set-variable -name "serverdir" -value "E:\RapidHIT ID"
-Get-ChildItem "$serverdir" -I storyboard*.* -R | Select-String "Critical diagnostics code"
-PAUSE
-Get-ChildItem "$serverdir"  -I  MachineConfig.xml -R | Select-String "MachineName", "HWVersion", "MachineConfiguration", "DataServerUploadPath", "FluidicHomeOffset",
-"PreMixHomeOffset", "DiluentHomeOffset", "SyringePumpStallCurrent", "SyringePumpResetCalibration", "LastBEC_TagID", "SpectrometerCalibration"
-Pause
-Get-ChildItem "$serverdir"  -I  TC_Calibration.xml -R | Select-String "<Offsets>GFE", "<Offsets>NGM"
-Pause
-Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Lysis Heater FAT", "DN FAT", "PCR FAT", "Optics Heater FAT", "Gel Cooling FAT", "Ambient FAT",
-"SCI Sensor/CAM FAT", "FRONT END FAT", "MEZZ test", "LP FAT", "HP FAT", "Anode Motor FAT", "BEC Interlock FAT", "Bring Up: Gel Antenna", "Syringe Stallout FAT", "Mezzboard FAT",
-"Piezo FAT", "HV FAT", "Laser FAT", "Bring Up: Verify Raman" , "Bring Up: Lysate Pull Test" , "Bolus Devliery Test", "Lysis Volume"
-pause
-Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Estimated gel void volume"
-pause
-<#
-Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Cartridge Type:     RHID_GFEControlCartridgePLUS",
-"Cartridge Type:     RHID_NGMSampleCartridgePLUS", "Cartridge Type:     RHID_GFESampleCartridgePLUS"
-pause
-#>
+
+
+Write-Host "config" 6>$null
+function config {
+    Get-ChildItem "$serverdir" -I  MachineConfig.xml, TC_Calibration.xml -R | Select-String "MachineName", "HWVersion", "MachineConfiguration", "DataServerUploadPath", "FluidicHomeOffset", 
+    "PreMixHomeOffset", "DiluentHomeOffset", "SyringePumpStallCurrent", "SyringePumpResetCalibration", "LastBEC_TagID", "double", "RunsSinceLastGelFill", "DeliveredSamples", "LaserHours", "Offsets"
+}
+
+Write-Host "d" 6>$null
+function d {
+    Get-ChildItem "$serverdir" -I  storyboard*.* -R | Select-String "Critical diagnostics code"
+}
+
+Write-Host "v" 6>$null
+function v {
+    Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Estimated gel void volume"
+}
+
+Write-Host "p" 6>$null
+function p {
+    Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Lysis Heater FAT", "DN FAT", "PCR FAT", "Optics Heater FAT", "Gel Cooling FAT", "Ambient FAT", 
+    "SCI Sensor/CAM FAT", "FRONT END FAT","Bring Up: FE Motor Calibration","Bring Up: FE Motor Test","Bring Up: Homing Error Test","Bring Up: FL Homing Error w/CAM Test","SCI Antenna Test", "MEZZ test", "LP FAT", "HP FAT", "Anode Motor FAT", "BEC Interlock FAT", "Bring Up: Gel Antenna", "Syringe Stallout FAT", "Mezzboard FAT",
+    "Piezo FAT", "HV FAT", "Laser FAT", "Bring Up: Verify Raman" , "Bring Up: Lysate Pull Test" , "Bolus Devliery Test", "Lysis Volume", "Bring Up: Water Prime", "Bring Up: Buffer Prime"
+}
+config
+"Enter 'd' to show Critical Diagnostic Code"
+"Enter 'v' to show Gel Void Volume"
+"Enter 'p' to show Test Progress"
+"Enter d;p;v (semi colon included) to show all "
+
+<#$commands = {
+
+    Write-Host "do some work"
+
+    $again = Read-Host "again?"
+    if ($again -eq "y"){
+        &$commands
+    } else {
+        Write-Host "end"    
+    }
+}
+
+&$commands #>
--- autoFAT Test result generator.ps1
+++ autoFAT Test result generator.ps1
@@ -1,35 +1,66 @@
-$sn = read-host "Please enter serial number"
-set-variable -name "serverdir" -value "E:\RHID-$sn"
 
-Write-Host "config" -InformationAction Ignore
-function config() {
-Get-ChildItem "$serverdir"  -I  MachineConfig.xml, TC_Calibration.xml -R | Select-String "MachineName", "HWVersion", "MachineConfiguration", "DataServerUploadPath", "FluidicHomeOffset", 
-    "PreMixHomeOffset", "DiluentHomeOffset", "SyringePumpStallCurrent", "SyringePumpResetCalibration", "LastBEC_TagID", "double", "Signature", "IsLinesPrimed", "LaserHours", "Offsets", "Water", "LysisBuffer", "LastGelPurgeOK", "DeliveredSamples"
-}
-Write-Host "essential" -InformationAction Ignore
-function essential() {
-Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Lysis Volume", "Ambient FAT"
-}
-Write-Host "d" -InformationAction Ignore
-function d() {
-    Get-ChildItem "$serverdir" -I  storyboard*.* -R | Select-String "Critical diagnostics code"
+if ($env:COMPUTERNAME = "SGSI11-59FKK13") {
+    set-variable -name "path" -value "S:\RHID"
+        } else {
+        } (set-variable -name "path" -value "E:\RHID")
+        $sn = read-host "Enter Insutrment Serial Number, enter again to paste folder path"
+
+if ($sn -eq '') {
+  ($sn = read-host "Enter Folder Path") -and (set-variable -name "serverdir" -value "$sn")
+} else {
+    (set-variable -name "serverdir" -value "$path-$sn")
 }
 
-Write-Host "v" -InformationAction Ignore
-function v() {
-    Get-ChildItem "$serverdir" -I  storyboard*.* -R | Select-String "Estimated gel void volume"
+Write-Host "config" 6>$null
+function config {
+    Get-ChildItem "$serverdir" -I  MachineConfig.xml -R | Select-String "MachineName", "HWVersion", "MachineConfiguration", "DataServerUploadPath", "FluidicHomeOffset", 
+    "PreMixHomeOffset", "DiluentHomeOffset", "SyringePumpStallCurrent", "SyringePumpResetCalibration", "LastBEC_TagID", "double", "RunsSinceLastGelFill", "DeliveredSamples", "LaserHours"
 }
 
-Write-Host "p" -InformationAction Ignore
-function p() {
-    Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Lysis Heater FAT", "DN FAT", "PCR FAT", "Optics Heater FAT", "Gel Cooling FAT", 
-    "SCI Sensor/CAM FAT", "FRONT END FAT", "MEZZ test", "LP FAT", "HP FAT", "Anode Motor FAT", "BEC Interlock FAT", "Bring Up: Gel Antenna", "Syringe Stallout FAT", "Mezzboard FAT",
-    "Piezo FAT", "HV FAT", "Laser FAT", "Bring Up: Verify Raman" , "Bring Up: Lysate Pull Test" , "Bolus Devliery Test"
+Write-Host "tc" 6>$null
+function tc {
+    Get-ChildItem "$serverdir" -I TC_Calibration.xml -R | Select-String "<Offsets>"
 }
-config;essential
-<#set-Alias -Scope global -name diag -Value d
-Set-Alias -Scope global -name void -Value v
-Set-Alias -Scope global -name progress -Value p
-config-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Cartridge Type:     RHID_GFEControlCartridgePLUS",
-vCarvoidge Type:     RHID_NGMSampleCartridgePLUS","Cartridge Type:     RHID_GFESampleCartridgePLUS"
+
+Write-Host "d" 6>$null
+function d {
+Get-ChildItem "$serverdir" -I  storyboard*.* -R | Select-String "SBH3","SBH4","BV2"
+}
+
+Write-Host "v" 6>$null
+function v {
+Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Estimated gel void volume"
+}
+
+
+Write-Host "p" 6>$null
+function p {
+Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Lysis Heater FAT", "DN FAT", "PCR FAT", "Optics Heater FAT", "Gel Cooling FAT","Ambient FAT", 
+"SCI Sensor/CAM FAT", "FRONT END FAT", "MEZZ test", "LP FAT", "HP FAT", "Anode Motor FAT", "BEC Interlock FAT", "Bring Up: Gel Antenna", "Syringe Stallout FAT", "Mezzboard FAT",
+ "Piezo FAT", "HV FAT", "Laser FAT", "Bring Up: Verify Raman" , "Bring Up: Lysate Pull Test" , "Bolus Devliery Test", "Lysis Volume", "Bring Up: Water Prime", "Bring Up: Buffer Prime"
+}
+
+config;tc
+"Enter 'd' to show Critical Diagnostic Code"
+"Enter 'v' to show Gel Void Volume"
+"Enter 'p' to show Test Progress"   
+"Enter d;p;v (with semi colon) to show all"
+
+
+<#
+Get-ChildItem "$serverdir"  -I  storyboard*.* -R | Select-String "Cartridge Type:     RHID_GFEControlCartridgePLUS",
+"Cartridge Type:     RHID_NGMSampleCartridgePLUS","Cartridge Type:     RHID_GFESampleCartridgePLUS"
 #>
+<#$commands = {
+
+    Write-Host "do some work"
+
+    $again = Read-Host "again?"
+    if ($again -eq "y"){
+        &$commands
+    } else {
+        Write-Host "end"    
+    }
+}
+
+&$commands #>
